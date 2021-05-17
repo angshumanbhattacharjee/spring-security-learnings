e81596cad0d385a21b162c8b112f23362111bdaf
angshumanbhattacharjee <angshumanbhattacharjee80@gmail.com>
2021-05-17 16:02:42 +0530

    Initial Commit

---
+++ spring-security-oauth/src/test/java/com/learning/securityoauth/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.securityoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
